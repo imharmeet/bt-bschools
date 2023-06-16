bbbd4bce0b31c82f794bb90cd3457d48e3e0124c
Pankaj Singh Negi <Pankaj.Negi@aajtak.com>
2023-06-16 11:34:47 +0530

    Worked on charts

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -139,4 +139,84 @@ var swiper = new Swiper(".mba-ranking-carousel", {
 });
 
 
+// Performance Indicators
+var dataADS = [
+  ['2020', '2021', '2022', '2023'],
+  // ['2023', '2022', '2021', '2020'],
+  [1920761, 1843200, 1855900, 1802000],
+  [1770344, 1729309, 1688737, 1640249],
+  [1442614, 1424389, 1397622, 1338963],
+  [1147003, 1146232, 1110082, 1069016],
+];
+var dataCF = [
+  // ['2020', '2021', '2022', '2023'],
+  ['2023', '2022', '2021', '2020'],
 
+  [29.63, 30.48, 25.49, 24.49],
+  [22.54, 23.53, 19.63, 19.61],
+  [17.01, 17.62, 14.78, 14.71],
+  [12.29, 12.68, 10.77, 10.64],
+];
+var dataMTFR = [
+  ['2022-23', '2021-22', '2020-21', '2019-20'],
+  [2.3, 2.1, 2.2, 2.1],
+  [2, 1.9, 2.1, 2.3],
+  [2, 1.9, 2, 2],
+  [1.6, 1.6, 1.6, 1.7],
+];
+var dataBS = [
+  // ['2020', '2021', '2022', '2023'],
+  ['2023', '2022', '2021', '2020'],
+
+  [438, 423, 397, 413],
+  [373, 377, 369, 340],
+  [286, 291, 285, 264],
+  [233, 240, 235, 226],
+];
+
+$(function () {
+
+  // Initial chart
+  var chart = c3.generate({
+    data: {
+      rows: dataADS,
+      type: 'bar',
+    },
+    axis: {
+      y: {
+        label: {
+          text: '₹ lakhs',
+          position: 'outer-right'
+        }
+      },
+      x: {
+        tick: {
+          categories: ['Top 10', 'Top 25', 'Top 50', 'Top 100']
+        },
+      },
+      rotated: true
+    },
+    color: {
+      pattern: ['#BEE296', '#89C597', '#549EA3', '#37749B']
+    },
+
+  });
+
+  // Redraw chart depending on which option is selected
+  $("#DataType").change(function (evt) {
+    var timeSelection = eval($("#DataType").val());
+    var chart = c3.generate({
+      data: {
+        rows: timeSelection,
+        type: 'bar'
+      },
+      axis: {
+        rotated: true
+      },
+      color: {
+        pattern: ['#BEE296', '#89C597', '#549EA3', '#37749B']
+      }
+    });
+  });
+
+});
